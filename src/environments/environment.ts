5bb9e70b7ed2163ae30f7665b278ec6ca216da5f
RanggaAns12 <ranggalifia19@gmail.com>
2026-04-08 14:08:30 +0700

    perbaikan env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.1.31:8000/api' 
-  // apiUrl: 'http://127.0.0.1:8000/api' 
+  apiUrl: 'ads-payroll.agrodeliserdang.com/api',
 };
