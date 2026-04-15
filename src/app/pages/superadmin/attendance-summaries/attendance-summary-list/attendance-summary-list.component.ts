9b9f36645134e9b9201a6389cb078f8b58bc1c88
RanggaAns12 <ranggalifia19@gmail.com>
2026-04-15 13:43:11 +0700

    push getTotal

--- src/app/pages/superadmin/attendance-summaries/attendance-summary-list/attendance-summary-list.component.ts
+++ src/app/pages/superadmin/attendance-summaries/attendance-summary-list/attendance-summary-list.component.ts
@@ -219,14 +219,14 @@ export class AttendanceSummaryListComponent implements OnInit {
   // =====================================================================
   getTotalIzin(item: any): number {
     return (
-      (item.izin_tidak_masuk_pribadi || 0) +
-      (item.izin_pulang_awal_pribadi || 0) +
-      (item.izin_datang_terlambat_pribadi || 0) +
-      (item.izin_meninggalkan_tempat_kerja || 0) +
-      (item.izin_dinas || 0) +
-      (item.izin_datang_terlambat_kantor || 0) +
-      (item.izin_pulang_awal_kantor || 0) +
-      (item.izin_lain_lain || 0)
+      (item.izin_tidak_masuk_pribadi || 0) 
+      // (item.izin_pulang_awal_pribadi || 0) +
+      // (item.izin_datang_terlambat_pribadi || 0) +
+      // (item.izin_meninggalkan_tempat_kerja || 0) +
+      // (item.izin_dinas || 0) +
+      // (item.izin_datang_terlambat_kantor || 0) +
+      // (item.izin_pulang_awal_kantor || 0) +
+      // (item.izin_lain_lain || 0)
     );
   }
 
